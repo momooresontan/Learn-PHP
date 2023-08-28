d0b3a28620b32bc4b9570a2cdea032a18fa1c840
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-08-28 10:13:43 +0100

    form multi submit & basic calculator

--- site.php
+++ site.php
@@ -6,14 +6,24 @@
     <title>Document</title>
 </head>
 <body>
-    <form action="site.php" method="get">
-        <input type="text" name="name" placeholder="Name">
+    <!-- <form action="site.php" method="get">
+        <input type="text" name="name" placeholder="Name"><br>
+        <input type="number" name="age" placeholder="Age"><br>
+        <input type="email" name="email" placeholder="Email">
         <input type="submit" >
+    </form> -->
+    <form action="site.php" method="get">
+        <input type="number" name="num1" placeholder="Number 1"><br>
+        <input type="number" name="num2" placeholder="Number 2"><br>
+        <input type="submit">
     </form>
+
     <br>
     <?php 
         //echo floor(2.9);
-        echo $_GET["name"];
+        // echo $_GET["name"], "<br>";
+        // echo $_GET["age"], "<br>";
+        // echo $_GET["email"];
     ?>
 
 </body>
