6efad0c2cb0887f5c2917ecc878bee8158f197e3
Momooreoluwa Sontan <sontanmomo@gmail.com>
2023-08-30 00:18:00 +0100

    getters & setters

--- site.php
+++ site.php
@@ -205,30 +205,41 @@
         // echo $book2->title;
         // echo $book2->author;
         // echo $book2->pages;
+        //Object function
+        // class Student {
+        //     var $name;
+        //     var $major;
+        //     var $gpa;
 
-        class Student {
-            var $name;
-            var $major;
-            var $gpa;
+        //     function __construct($name, $major, $gpa){
+        //         $this->name = $name;
+        //         $this->major = $major;
+        //         $this->gpa = $gpa;
+        //     }
 
-            function __construct($name, $major, $gpa){
-                $this->name = $name;
-                $this->major = $major;
-                $this->gpa = $gpa;
-            }
+        //     function hasHonours(){
+        //         if($this->gpa >= 3.5){
+        //             return "true";
+        //         }
+        //         return "false";
+        //     }
+        // }
+        // $student1 = new Student("Jim", "Business", 3.4);
+        // $student2 = new Student("Pam", "Art", 3.6);
 
-            function hasHonours(){
-                if($this->gpa >= 3.5){
-                    return "true";
-                }
-                return "false";
+        // echo $student2->hasHonours();
+
+        class Movie {
+            public $title;
+            public $rating;
+
+            function __construct($title, $rating){
+                $this->title = $title;
+                $this->rating = $rating;
             }
         }
-        $student1 = new Student("Jim", "Business", 3.4);
-        $student2 = new Student("Pam", "Art", 3.6);
-
-        echo $student2->hasHonours();
-
+        $avengers = new Movie("Avengers", "PG-13");
+        echo $avengers->rating;
     ?>
 
 </body>
